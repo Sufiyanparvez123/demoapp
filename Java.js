9f62e59e38ff0164f5994bf937c1e2b608963c49
Sufiyanparvez123 <84910423+Sufiyanparvez123@users.noreply.github.com>
2021-10-10 12:20:30 +0530

    Update Java.js

--- Java.js
+++ Java.js
@@ -4,4 +4,12 @@ document.write('Hello world')
 document.write('Hello world')
 document.write('Hello world')
 document.write('Hello world')
+
+document.write('Hello world')
+
+document.write('Hello world')
+
+document.write('Hello world')
+
+document.write('Hello world')
 </script>
